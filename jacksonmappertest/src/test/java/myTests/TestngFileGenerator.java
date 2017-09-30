53037916b14515ccface9ec0e5b2132b80a7d7ff
karthic.1888@gmail.com <Karthic@KarthicKomp>
2017-09-30 10:17:36 +0530

    java comments

--- jacksonmappertest/src/test/java/myTests/TestngFileGenerator.java
+++ jacksonmappertest/src/test/java/myTests/TestngFileGenerator.java
@@ -10,3 +10,17 @@ public class TestngFileGenerator {
 	}
 
 }
+package myTests;
+
+import utilities.XLSReader;
+
+public class TestngFileGenerator {
+	/*
+	 * Will create a TestNG xml file dynamically from an excel data//.
+	 */
+	public static void main(String[] args) {
+		XLSReader suite = new XLSReader("src/main/resources/Test.xlsx");
+		suite.getTests("select * from TestCase where module='index'");
+	}
+
+}
